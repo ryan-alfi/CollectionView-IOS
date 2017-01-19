daf2316b4e34cb91498d65ed72d60b89ce1cb5ec
Andi Septiadi <andi@dycode.com>
2017-01-19 17:58:12 +0700

    imac Commit

---
+++ UICollectionViewRyan/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  UICollectionViewRyan
+//
+//  Created by iMac on 1/18/17.
+//  Copyright © 2017 Dycode. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+}
--- UICollectionViewRyan/ViewController.swift
+++ UICollectionViewRyan/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    var days = ["Senin","Selasa","Rabu","Kamis", "Jum'at","Sabtu","Minggu"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return days.count
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.title.text = days[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath.item % 3 == 0{
+            return CGSize(width: collectionView.frame.width - 16, height: 100)
+        }else{
+            return CGSize(width: (collectionView.frame.width - 40) / 2, height: 100)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
+    }
+    
 }
 
